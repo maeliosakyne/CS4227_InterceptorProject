49937881ae4d8adb78a15a0609155db43eb44aa0
MAELIOSA KYNE <19232624@studentmail.ul.ie>
2023-02-20 16:38:23 +0000

    java classes added

---
+++ src/Movie.java
@@ -0,0 +1,20 @@
+public class Movie {
+    
+    public static final int REGULAR = 0;
+
+    private final String title;
+    private final int priceCode;
+
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPriceCode() {
+        return priceCode;
+    }
+}
---
+++ src/RegularPrice.java
@@ -0,0 +1,11 @@
+public class RegularPrice extends Price {
+   
+    @Override
+    public double getCharge(int daysRented) {
+        double result = 2.0;
+        if (daysRented > 2) {
+            result += (daysRented - 2) * 1.5;
+        }
+        return result;
+    }
+}
