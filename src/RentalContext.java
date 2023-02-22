af5201811ba25660d2e9bf285aca10e3d0fa627f
MAELIOSA KYNE <19232624@studentmail.ul.ie>
2023-02-22 11:15:28 +0000

    RentalContext.java file and loggingInterceptor.java added

---
+++ src/RentalContext.java
@@ -0,0 +1,38 @@
+public class RentalContext {
+	//declare the instances of Customer, movie and price classes
+    private final Customer customer;
+    private final Movie movie;
+    private final Price price;
+    //declare a constant variable named daysRented
+    private final int daysRented;
+
+    //constructor passing through 4 parameters, customer, movie, price, daysRented
+    public RentalContext(Customer customer, Movie movie, Price price, int daysRented) {
+        this.customer = customer;
+        this.movie = movie;
+        this.price = price;
+        this.daysRented = daysRented;
+    }
+    
+    //get the customer
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    //get the movie
+    public Movie getMovie() {
+        return movie;
+    }
+    
+    //get the price
+
+    public Price getPrice() {
+        return price;
+    }
+
+    
+    //get the days rented
+    public int getDaysRented() {
+        return daysRented;
+    }
+}
