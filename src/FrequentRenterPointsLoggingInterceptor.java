f96cbe5661ea398dad6774b110e3ec0407065271
MAELIOSA KYNE <19232624@studentmail.ul.ie>
2023-02-26 23:13:24 +0000

    Main.java fixed

--- src/FrequentRenterPointsLoggingInterceptor.java
+++ src/FrequentRenterPointsLoggingInterceptor.java
@@ -3,7 +3,7 @@ public class FrequentRenterPointsLoggingInterceptor implements LoggingIntercepto
     @Override
     // call log function from Logginginterceptor using override
     public void log(String message){
-        System.out.println("Frequent Renter Points" + message);
+        System.out.println("Frequent Renter Points ---- " + message);
     }
 
 }
